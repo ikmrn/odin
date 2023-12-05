93958e17887357591a50defb25d20e061ca12514
ikmrn <ibragimovkmrn@gmail.com>
2023-12-05 23:30:46 +0500

    chore(game): Improve comments for clarity

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,12 +1,12 @@
 // Add valid options
 const validOptions = ["Rock", "Paper", "Scissors"];
 
-// Get computer choice
+// Get computer choice, a random value from 0 to 2
 function getComputerChoice() {
   return Math.floor(Math.random() * 3);
 }
 
-// Check input function
+// Check input for valid type and return capitalized input 
 function checkInput(input) {
   if (typeof input !== "string") {
     return "Invalid input. Please provide a string.🎃";
@@ -16,11 +16,9 @@ function checkInput(input) {
   if (validOptions.includes(inputCap)) {
     return inputCap;
   }
-  console.log("Choose from Rock, Paper, or Scissors.");
   return checkInput(prompt("Choose from Rock, Paper, or Scissors.", ""));
 }
 
-// Get player choice
 function getPlayerChoice(n = 0) {
   if (n) {
     let playerSelection = checkInput(
@@ -35,12 +33,14 @@ function getPlayerChoice(n = 0) {
 
 // Play the game
 function playRound(playerChoice, computerChoice) {
-  // Calculate game result
+  // Calculate game result using the Rock, Paper, Scissors logic.
+  // The result will be 0 for a tie, 1 for player win, and 2 for computer win.
+  // Step 1: Calculate the numeric difference between player and computer choices.
+  // Step 2: Ensure the result is always non-negative by adding 3.
+  // Step 3: Use modulo 3 to confine the result to 0, 1, or 2.
   let gameResult = (playerChoice - computerChoice + 3) % 3;
-  console.log(`Game result: ${gameResult}`);
 
   // Determine the winner
-  //
   if (gameResult === 0) {
     return playRound(getPlayerChoice(1), getComputerChoice());
   } else if (gameResult === 1) {
@@ -56,14 +56,21 @@ function playRound(playerChoice, computerChoice) {
   }
 }
 
-function game(numberOfGames) {
+function playGame(numberOfGames) {
   let gameResult = 0;
-  for (let i = 0; i < 5; i++) {
-    let round = playRound(getPlayerChoice(), getComputerChoice());
-    let roundResult = round.result;
+  for (let i = 0; i < parseInt(numberOfGames); i++) {
+    // Play one round
+    let oneRound = playRound(getPlayerChoice(), getComputerChoice());
+
+    // Retrieve the result and the message
+    let roundResult = oneRound.result;
+    let roundMessage = oneRound.message;
+
     gameResult += roundResult;
     console.log(gameResult);
+    console.log(roundMessage)
   }
+  console.log(`Overall score: ${gameResult}`)
 }
 
-game(5);
+playGame(5);
